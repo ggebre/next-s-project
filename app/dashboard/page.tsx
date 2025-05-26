1af57072d001b3acca7c41edd294d262c02cd65f
Getu Gebre <getu.gebre.gg@gmail.com>
2025-05-25 21:24:43 -0500

    update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,9 @@ import { Card } from "../ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-
-export default function Page() {
-
+import { fetchRevenue } from "../lib/data";
+export default async function Page() {
+    const revenue = await fetchRevenue();
     return (
         <main>
             <h1 className={ `${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -22,7 +22,7 @@ export default function Page() {
                 /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {/* <RevenueChart revenue={revenue}  /> */}
+                <RevenueChart revenue={revenue}  />
                 {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
             </div>
         </main>
